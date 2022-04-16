9606dd8d5c002538143e25b2ccb5716c5bc6a2f3
z8kh8E6t rEv62qT7 <23183218+z8kh8E6t-rEv62qT7@users.noreply.github.com>
2022-04-17 02:11:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-CXX=clang++ CC=clang CGO_ENABLED=1 go build --tags extended -o ./build/hugo -trimpath -ldflags '-s -w -linkmode "external" -extldflags "-static"' .
+CXX=g++ CC=gcc CGO_ENABLED=1 go build --tags extended -o ./build/hugo -trimpath -ldflags '-s -w -linkmode "external" -extldflags "-static"' .
